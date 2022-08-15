c0adb3090db7ac06c5a269e04141188016b0b599
inanakalin <111204187+inanakalin@users.noreply.github.com>
2022-08-15 21:44:56 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int time = DateTime.Now.Hour;
+
+        if(time>=21 && time<24)
+        Console.WriteLine("İyi uykular");
+
+        else if(time<=18)
+        Console.WriteLine("iyi Günler");
+
+        else
+        Console.WriteLine("iyi Geceler");
+
+        string sonuc = time<18 ? "İyi Günler" : "İyi Geceler";
+
+        sonuc = time>=21 && time<=24 ? "İyi Uykular" : time<18 ? "İyi Günler" : "İyi Geceler";
+        Console.WriteLine(sonuc);
+
+
+
+    }
+}
